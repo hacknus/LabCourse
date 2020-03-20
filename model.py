c209bf4c1437995982e8bedf991b2301b7c9c8f6
Linus <l_stoeckli@bluewin.ch>
2020-03-20 15:11:02 +0100

    first python file

---
+++ model.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+
+class Simulation:
+
+	def __init__(self):
+		pass
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	pass
