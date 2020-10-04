be6012c5410403e1849210810a415edb3f10ce8b
hacknus <l_stoeckli@bluewin.ch>
2020-10-04 18:57:56 +0200

    adapted to work with Windows and UNIX-like systems

--- Radiometer/data_analysis.py
+++ Radiometer/data_analysis.py
@@ -2,10 +2,18 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import platform
 
-angles = pd.read_csv (r'16GHZ\angles.csv')
-hot_load_before = pd.read_csv (r'16GHZ\hot_load_before.csv')
-hot_load_after = pd.read_csv (r'16GHZ\hot_load_after.csv')
+OS = platform.system()
+
+if OS == "Windows":
+    angles = pd.read_csv(r'16GHZ\angles.csv')
+    hot_load_before = pd.read_csv(r'16GHZ\hot_load_before.csv')
+    hot_load_after = pd.read_csv(r'16GHZ\hot_load_after.csv')
+else:
+    angles = pd.read_csv('16GHZ/angles.csv')
+    hot_load_before = pd.read_csv('16GHZ/hot_load_before.csv')
+    hot_load_after = pd.read_csv('16GHZ/hot_load_after.csv')
 
 k=0                     #wie viele datenpunkte weglassen
 T0=2.7 + 273.15
@@ -47,5 +55,5 @@ while np.abs(B0) >= 0.01:
 
 plt.plot(f)
 plt.plot(rel_thickness,tau)
-plt.show
+plt.show()
 print(tau0)
