4719f11087e50c8082bec3d66d5e74cc43e47a4f
donathess <62433205+donathess@users.noreply.github.com>
2020-10-04 10:25:17 +0200

    Update data_analysis.py

--- Radiometer/data_analysis.py
+++ Radiometer/data_analysis.py
@@ -8,7 +8,7 @@ hot_load_before = pd.read_csv (r'16GHZ\hot_load_before.csv')
 hot_load_after = pd.read_csv (r'16GHZ\hot_load_after.csv')
 
 k=0                     #wie viele datenpunkte weglassen
-T0=2.7
+T0=2.7 + 273.15
 tau0=0.3
 T_m = 16-10
 
@@ -23,18 +23,14 @@ def tau_function(tau0):
         U_teta = angles.v_val[i+k]
         T_teta = (U_teta-U_hot)/(U_hot-U_cold) * (T_hot-T_cold)+T_hot
         tau[i] = np.log((T_m-T0)/(T_m-T_teta))
-    print(tau[9-k])
     return tau
-
 
 rel_thickness = np.zeros(10-k)
 for i in np.arange(10-k):
     rel_thickness[i] = 1/np.cos((90-angles.ele_val[i+k])/180*np.pi)
-print(rel_thickness)
+
 B0=1
-s=-6
-while s <=1:                     #while np.abs(B0) >= 0.01:
-    s=s+1
+while np.abs(B0) >= 0.01:
     tau = tau_function(tau0)
     sum1=0
     sum2=0
@@ -42,12 +38,14 @@ while s <=1:                     #while np.abs(B0) >= 0.01:
         sum1 = sum1 + (rel_thickness[i]-np.mean(rel_thickness))*(tau[i]-np.mean(tau))
         sum2 = sum2 + (rel_thickness[i]-np.mean(rel_thickness))**2
     B1 = sum1/sum2
-    B0 = np.mean(tau) - B1* np.mean(rel_thickness)
-    plt.plot(rel_thickness,tau)
-    plt.show
+    B0 = np.mean(tau) - B1* np.mean(rel_thickness)    
     tau0 = B0 + B1*1
+    
     f= np.zeros(4)
     for i in np.arange(4):
         f[i] = B0 + B1*i
-    plt.plot(f)
+
+plt.plot(f)
+plt.plot(rel_thickness,tau)
+plt.show
 print(tau0)
