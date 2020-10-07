585afef13f4981d261d2024a572ae05f5d3550f1
donathess <62433205+donathess@users.noreply.github.com>
2020-10-07 19:12:05 +0200

    Update Sky_temperature.py

--- Radiometer/Sky_temperature.py
+++ Radiometer/Sky_temperature.py
@@ -2,23 +2,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import platform
-from data_analysis import K,c,rel_thickness
+
 def T(U,K,c):
-    return K*U+c
+    return K*U+C
 
 OS = platform.system()
-angles = pd.read_csv(r'{}GHZ\K_and_C.csv'.format(freq))
 for freq, c in zip([16, 17, 18, 19], ['red', 'blue', 'green', 'orange']):
     if OS == "Windows":
-        angles = pd.read_csv(r'{}GHZ\angles.csv'.format(freq))  
+        K_and_C = pd.read_csv(r'{}GHZ\K_and_C.csv'.format(freq))  
+        angles = pd.read_csv(r'{}GHZ\angles.csv'.format(freq))
     else:
-        angles = pd.read_csv('{}GHZ/angles.csv'.format(freq))
-    print(angles.v_val)
-    K=1
-    c=2
-    T_sky = T(angles.v_val,K,c)
+        K_and_C = pd.read_csv('{}GHZ/K_and_C.csv'.format(freq))
+        angles = pd.read_csv(r'{}GHZ/angles.csv'.format(freq))
     
+    K=np.array(K_and_C.K)
+    C=np.array(K_and_C.C)
+    T_sky = T(np.array(angles.v_val),K,C)
+    rel_thickness = 1 / np.cos((90 - np.array(angles.ele_val)) / 180 * np.pi)
     plt.plot(rel_thickness,T_sky)
+    
 plt.xlabel("rel. thickness [-]")
 plt.ylabel("T [$C°$]")
 plt.legend()
