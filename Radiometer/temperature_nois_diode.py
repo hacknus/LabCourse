f4b8e6ba4543e3fdc255b61e78f8fd8b5502dce9
donathess <62433205+donathess@users.noreply.github.com>
2020-10-08 20:12:01 +0200

    Update temperature_nois_diode.py

--- Radiometer/temperature_nois_diode.py
+++ Radiometer/temperature_nois_diode.py
@@ -5,8 +5,18 @@ import platform
 from main import mat_temp
 
 frequency= np.array([16, 17, 18, 19])+3.75+0.55
+T_diode = np.zeros(4)
+T_diode_err = np.zeros(4)
 for i in [16, 17, 18, 19]:
     df = pd.read_csv(r'{}GHZ\diode_only.csv'.format(i))
     params = pd.read_csv(r'{}GHZ\K_and_C.csv'.format(i))
-    T_diode, T_diode_err = mat_temp(df, params)
-    print(T_diode)
+    T_diode[i-16], T_diode_err[i-16] = mat_temp(df, params)
+plt.errorbar(frequency, T_diode, yerr=T_diode_err, color='black', fmt='.', markeredgecolor ="black",ecolor='black', capthick=2,capsize=2, elinewidth=1, markeredgewidth=0.5,   ms=5)
+plt.plot(frequency,T_diode,'.',color='black')
+plt.xticks(frequency)
+plt.ylabel('T [C°]')
+plt.xlabel('frequency [GHz]')
+#plt.legend()
+plt.tight_layout()
+plt.savefig("nois_diode.pdf")
+plt.show()
