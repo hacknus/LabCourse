ba724957c391839b19794d9e18c016a1430748b1
donathess <62433205+donathess@users.noreply.github.com>
2020-10-12 16:05:42 +0200

    Update Sky_temperature.py

--- Radiometer/Sky_temperature.py
+++ Radiometer/Sky_temperature.py
@@ -6,6 +6,8 @@ from main import sky_temp
 
 
 OS = platform.system()
+
+                
 for freq, c in zip([16, 17, 18, 19], ['red', 'blue', 'green', 'orange']):
     if OS == "Windows":
         K_and_C = pd.read_csv(r'{}GHZ\K_and_C.csv'.format(freq))  
@@ -19,7 +21,6 @@ for freq, c in zip([16, 17, 18, 19], ['red', 'blue', 'green', 'orange']):
         T_sky[i], T_sky_err[i] = sky_temp(angles,K_and_C,i)
     rel_thickness = 1 / np.cos((90 - np.array(angles.ele_val)) / 180 * np.pi)
     plt.errorbar(rel_thickness, T_sky+273.15, yerr=T_sky_err, color=c, fmt='.-', markeredgecolor ="black",label='{}GHz'.format(freq+4.3) ,ecolor='black', capthick=2,capsize=2, elinewidth=1, markeredgewidth=0.5,   ms=5)
-    
 plt.xlabel("rel. thickness [-]")
 plt.ylabel("T [K°]")
 plt.legend()
