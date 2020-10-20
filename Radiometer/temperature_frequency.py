7ff48abcf0f3737666dcb6a7ddc77e6d32f94a45
donathess <62433205+donathess@users.noreply.github.com>
2020-10-20 02:25:44 +0200

    Update temperature_frequency.py

--- Radiometer/temperature_frequency.py
+++ Radiometer/temperature_frequency.py
@@ -6,7 +6,7 @@ from main import sky_temp
 import csv
 
 with open('sky_temp.csv','w', newline='') as csvfile:
-    fieldnames = ['rel. thickness', 'rel. thickness_err', 'T_20 [K]', 'T_20_err [K]', 'T_21 [K]', 'T_21_err [K]', 'T_22 [K]', 'T_22_err [K]', 'T_23 [K]', 'T_23_err [K]']
+    fieldnames = ['rel.thickness', 'rel.thickness_err', 'T_20', 'T_20_err', 'T_21', 'T_21_err', 'T_22', 'T_22_err', 'T_23', 'T_23_err']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     zenith_angle = np.array((90 - np.array([17.9,19.5,21.3,23.6,26.4,30.0,34.8,41.8,53.1,90]))/180*np.pi)
@@ -25,7 +25,7 @@ with open('sky_temp.csv','w', newline='') as csvfile:
             plt.errorbar(frequency, T_freq+273.15, yerr=T_freq_err, label='{0:.2f}$\pm${1:.2f}'.format(rel_thickness[elevation],rel_thickness_err[elevation]), color=c, fmt='.-', markeredgecolor ="black",ecolor='black', capthick=2,capsize=2, elinewidth=1, markeredgewidth=0.5,   ms=5)
         T_freq = np.round(T_freq,2)
         T_freq_err = np.round(T_freq_err,2)   
-        writer.writerow({'rel. thickness':rel_thickness[elevation], 'rel. thickness_err':rel_thickness_err[elevation] , 'T_20 [K]': T_freq[0], 'T_20_err [K]': T_freq_err[0], 'T_21 [K]': T_freq[1], 'T_21_err [K]': T_freq_err[1], 'T_22 [K]': T_freq[2], 'T_22_err [K]': T_freq_err[2], 'T_23 [K]': T_freq[3], 'T_23_err [K]': T_freq_err[3]})
+        writer.writerow({'rel.thickness':rel_thickness[elevation], 'rel.thickness_err':rel_thickness_err[elevation] , 'T_20': T_freq[0], 'T_20_err': T_freq_err[0], 'T_21': T_freq[1], 'T_21_err': T_freq_err[1], 'T_22': T_freq[2], 'T_22_err': T_freq_err[2], 'T_23': T_freq[3], 'T_23_err': T_freq_err[3]})
 plt.legend(title='thickness')
 plt.ylabel('T [K°]')
 plt.xticks(frequency)
