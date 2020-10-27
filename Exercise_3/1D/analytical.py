8499207b3dee5c525a4b8d04262c4c6cf09e7280
donathess <62433205+donathess@users.noreply.github.com>
2020-10-27 20:08:21 +0100

    Create analytical.py

---
+++ Exercise_3/1D/analytical.py
@@ -0,0 +1,18 @@
+import numpy as np
+from constants import u0,std,a
+
+
+def analytical_solution_dirichlet(t,x):
+    return u0*std/np.sqrt(std**2+4*a*t)*np.e**((x**2/(4*a*t))*(std**2/(std**2+4*a*t)-1))
+
+def analytical_dirichlet(delta_z,delta_t,number_z,number_t):
+    grid_z = np.array(delta_z*np.arange(number_z))
+    grid_t = np.array(delta_t*np.arange(number_t))
+    grid = np.zeros((number_t,number_z))
+    for i in np.arange(number_t):
+        if i*delta_t < 0.01:
+            grid[i]=0
+        else:
+            grid[i]=analytical_solution_dirichlet(grid_t[i],grid_z-0.5*(number_z-1)*delta_z)
+    grid = np.transpose(grid)
+    return grid
