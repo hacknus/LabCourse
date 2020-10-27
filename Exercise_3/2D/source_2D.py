5b2fd094ec35653f77eda4c6b5c7a429dd40ee59
donathess <62433205+donathess@users.noreply.github.com>
2020-10-27 23:10:42 +0100

    Delete source_2D.py

--- Exercise_3/2D/source_2D.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from constants_2D import power,rel_x,rel_y
-
-
-def source(number_z):
-    S = np.zeros(number_z**2)
-    S[int(rel_x*(number_z-1)*number_z)+int((number_z-1)*rel_y)]=power
-    S[int((rel_x*(number_z-1)+1)*number_z)+int((number_z-1)*rel_y)]
-    return S
