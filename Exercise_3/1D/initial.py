e87b8a8c7cd8e7872d6b733dc180a50b7c16f4f4
donathess <62433205+donathess@users.noreply.github.com>
2020-10-28 01:02:15 +0100

    Update initial.py

--- Exercise_3/1D/initial.py
+++ Exercise_3/1D/initial.py
@@ -1,19 +1,18 @@
 import numpy as np
-from constants import u0, std
 
 
 
-def initial_analyticaly(x,delta_z,number_z):
+
+def initial_analyticaly(x,delta_z,number_z,u0,std):
     return u0*np.exp(-x**2/std**2)
 
-class normal:
-    def T0(number_z,delta_z):
-        grid = np.array(delta_z*np.arange(number_z))
-        grid = grid-0.5*(number_z-1)*delta_z
-        return initial_analyticaly(grid,delta_z,number_z)
+def initial_normal(number_z,delta_z,u0,std):
+    grid = np.array(delta_z*np.arange(number_z))
+    grid = grid-0.5*(number_z-1)*delta_z
+    return initial_analyticaly(grid,delta_z,number_z,u0,std)
 
-class const:
-    def T0(number_z,delta_z):
-        grid = np.array(delta_z*np.arange(number_z))
-        return grid*0
+
+def initial_const(number_z,delta_z):
+    grid = np.array(delta_z*np.arange(number_z))
+    return grid*0
         
