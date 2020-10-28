a6e93f84189fc4d3e056008c2948d348d351e369
donathess <62433205+donathess@users.noreply.github.com>
2020-10-28 01:02:07 +0100

    Update boundary.py

--- Exercise_3/1D/boundary.py
+++ Exercise_3/1D/boundary.py
@@ -1,5 +1,5 @@
 import numpy as np
-from constants import const_dirichlet, const_neumann, const_cauchy_a, const_cauchy_b, const_cauchy_c
+
 
 
 
@@ -13,7 +13,7 @@ class dirichlet:
         A[-1][-2]=0
         return A
     
-    def C(number_z,delta_z):
+    def C(number_z,delta_z,const_dirichlet):
         C=np.zeros(number_z)
         C[0]=const_dirichlet
         C[-1]=const_dirichlet
@@ -28,21 +28,21 @@ class neumann:
         A[-1][-1]=1-r
         return A
     
-    def C(number_z,delta_z):
+    def C(number_z,delta_z,const_neumann):
         C = np.zeros(number_z, dtype=np.float)
         C[0] = 0.5*delta_z*const_neumann
         C[-1]= 0.5*delta_z*const_neumann
         return C
     
 class cauchy:
-    def A(number_z,delta_z):
+    def A(number_z,delta_z,const_cauchy_a,const_cauchy_b):
         r=0.5
         A=np.diagflat(np.full(number_z, 1-2*r, dtype=np.float))+np.diagflat(np.full(number_z-1, r, dtype=np.float),1)+np.diagflat(np.full(number_z-1, r, dtype=np.float),-1)
         A[0][0]=(1-r*(const_cauchy_a/const_cauchy_b*delta_z+1))
         A[-1][-1]=(1-r*(const_cauchy_a/const_cauchy_b*delta_z+1))
         return A
     
-    def C(number_z,delta_z):
+    def C(number_z,delta_z,const_cauchy_c,const_cauchy_b):
         C = np.zeros(number_z, dtype=np.float)
         C[0] = 0.5*delta_z*const_cauchy_c/const_cauchy_b
         C[-1]= 0.5*delta_z*const_cauchy_c/const_cauchy_b
